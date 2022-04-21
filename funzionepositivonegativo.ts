6364de3126706ea15ce1d86fa2c7a84e16bd6339
Pasquale <pasqualecerra96@gmail.com>
2022-04-21 14:25:40 +0200

    positivo negativo

---
+++ funzionepositivonegativo.ts
@@ -0,0 +1,9 @@
+function positivonegativo(x:number){
+    if(x>0){
+    return console.log("il numero è positivo");
+}else if(x==0){console.log("il numero non è ne positivo ne negativo");
+
+}else return console.log("il numero è negativo");
+}
+let x:number=prompt("inserisci numero");
+positivonegativo(x); 
