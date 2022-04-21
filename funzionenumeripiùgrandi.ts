a124015c1962af90241bff9ecdec74a28fd29431
Pasquale <pasqualecerra96@gmail.com>
2022-04-21 16:40:39 +0200

    sistemato funzione

--- funzionenumeripiùgrandi.ts
+++ funzionenumeripiùgrandi.ts
@@ -1,15 +1,15 @@
 function maggiore(a:number,b:number,c:number){
-if(a*1>b&&a*1>c){
-    return console.log(a)
-}
-else if(b*1>c){
-    return console.log(b)
-}else return console.log(c)
-
-}
-
-let d :number=prompt("inserisci numero a"); 
-let e :number=prompt("inserisci numero b"); 
-let f :number=prompt("inserisci numero c");
-
-maggiore(d,e,f);
+    if(a*1>b&&a*1>c){
+        return a
+    }
+    else if(b*1>c){
+        return b
+    }else return c
+    
+    }
+    
+    let d :number=prompt("inserisci numero a"); 
+    let e :number=prompt("inserisci numero b"); 
+    let f :number=prompt("inserisci numero c");
+    
+    console.log(maggiore(d,e,f));
