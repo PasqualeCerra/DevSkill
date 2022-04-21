01332a7c91a5d9a8ba770f9c097c38a541c8cffc
Pasquale <pasqualecerra96@gmail.com>
2022-04-21 14:25:25 +0200

    pari o dispari

---
+++ funzioneparidispari.ts
@@ -0,0 +1,9 @@
+function paridispari(a:number){
+    if(a%2==0){
+    return console.log("è pari");
+}else return console.log("è dispari")
+}
+let x:number=prompt("inserisci numero");
+paridispari(x); 
+
+
