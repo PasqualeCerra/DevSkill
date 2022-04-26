46e0069848314639392165145d49afb309a2d96d
Pasquale <pasqualecerra96@gmail.com>
2022-04-26 12:51:27 +0200

    usato vettore

--- funzionenumeripiùgrandi.ts
+++ funzionenumeripiùgrandi.ts
@@ -1,4 +1,4 @@
-function maggiore(a:number,b:number,c:number){
+/*function maggiore(a:number,b:number,c:number){
     if(a*1>b&&a*1>c){
         return a
     }
@@ -12,4 +12,15 @@ function maggiore(a:number,b:number,c:number){
     let e :number=prompt("inserisci numero b"); 
     let f :number=prompt("inserisci numero c");
     
-    console.log(maggiore(d,e,f));
+    console.log(maggiore(d,e,f));*/
+    let numeri:number[]=[];
+numeri[0]=1;
+numeri[1]=2;
+numeri[2]=3;
+
+if(numeri[0]>numeri[1]&& numeri[0]>numeri[2]){
+    console.log(numeri[0]);
+}
+else if(numeri[1]>numeri[2]){
+    console.log(numeri[1]);
+}else console.log(numeri[2]);
