f11bc8b8f6b5480443e1b0c191207f041bd46215
Pasquale <pasqualecerra96@gmail.com>
2022-04-29 12:12:03 +0200

    sistemato il "bug" della stampa dell'1 con una soluzione ridondante

--- primi30numeriprimi.ts
+++ primi30numeriprimi.ts
@@ -1,13 +1,13 @@
-let numero:number=1;
+let numero:number=0;
 let allarme:boolean=true;
-let divisore:number=1;
+let divisore:number=0;
 let contatore=0;
 while(contatore<30){
     while(allarme==true){
 
     if(numero%divisore==0&&numero!=divisore&&divisore!=1){
         allarme=false;
-    }else if(divisore==1&&allarme==true){
+    }else if(divisore==1&&allarme==true&&numero!=1){
         console.log(numero);
         allarme=false;
         contatore++;
